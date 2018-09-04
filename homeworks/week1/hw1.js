bb7557b24b10aaec89f04d8c4c5c204af3ba8c1b
papashong1111 <yypp06@gmail.com>
2018-09-04 21:12:10 +1000

    add hw1 & 2

--- homeworks/week1/hw1.js
+++ homeworks/week1/hw1.js
@@ -1,3 +1,8 @@
-function printStars(n) {
-  
+let i;
+let star = '';
+for (i = 1; i < 31; i++){
+    let data = 'n = ' + i;
+    star += '*';
+    console.log(data);
+    console.log(star);
 }
--- homeworks/week1/hw2.js
+++ homeworks/week1/hw2.js
@@ -1,3 +1,11 @@
 function capitalize(str) {
-  
+  var str = str.toLowerCase();
+  var strarr = str.split(' ');
+  var result = '';
+    for(var i in strarr){
+      result += strarr[i].substring(0,1).toUpperCase()+strarr[i].substring(1)+' ';
+    }
+  return result
 }
+console.log(capitalize('nnnnnn'));
+console.log(capitalize(',nnnnn'))
