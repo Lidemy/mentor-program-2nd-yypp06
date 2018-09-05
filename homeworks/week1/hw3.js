17439599def7bcb3808a749b42407d6331bf35bc
papashong1111 <yypp06@gmail.com>
2018-09-05 20:18:39 +1000

    add hw3

--- homeworks/week1/hw3.js
+++ homeworks/week1/hw3.js
@@ -1,3 +1,13 @@
-function reverse(str) {
-
+function String(str) {
+  splitString = str.split('');  
+  arr = [];
+  a = splitString.length - 1; 
+  for (i = a; i > -1; i--) { 
+    arr.push(splitString[i]);
+    
+  }
+  str = arr.join('');
+  return str;
 }
+
+console.log(String('goodday'));
