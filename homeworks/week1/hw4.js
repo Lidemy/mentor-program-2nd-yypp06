79af4294d49a8c3711050d29d7588c0778e42fe4
Neo <yypp06@gmail.com>
2018-09-07 13:56:25 +1000

    week1 hw done

--- homeworks/week1/hw4.js
+++ homeworks/week1/hw4.js
@@ -1,3 +1,9 @@
 function printFactor(n) {
-
+  for(i = 0; i <= n; i++){
+    if(n%i === 0){
+      console.log(i)
+    }   
+  } 
 }
+
+printFactor(10)
--- homeworks/week1/hw5.js
+++ homeworks/week1/hw5.js
@@ -1,7 +1,22 @@
 function join(str, concatStr) {
-
-}
-
+  let newStr = str[0]; 
+  for (i = 0; i < str.length; i++){
+    newStr += concatStr + str[i]
+  }
+  return newStr
+ }
+ console.log(join([1,2,3],'!'))
+ console.log(join([1,2,3],'a'))
+   
+    
+    
 function repeat(str, times) {
+  let newStr = str[0]; 
+  for (i = 0; i < times; i++){
+    newStr += str
+  }
+  return newStr 
+ }
 
-}
+console.log(repeat('a',5))
+console.log(repeat('yoyoyo',5))
